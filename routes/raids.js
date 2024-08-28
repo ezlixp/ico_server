af99d6387b6ceed8aff42bfb61190fa4144eeda8
helooo1233 <71606730+helooo1233@users.noreply.github.com>
2024-08-27 22:40:19 -0400

    chore: add validateJwtToken middleware to raid endpoints and add .env.example

--- routes/raids.js
+++ routes/raids.js
@@ -2,62 +2,62 @@
 import validateJwtToken from "../security/jwtTokenValidator.js";
 
 function mapRaidEndpoints(app) {
-    app.get("/getRaids", validateJwtToken, async (request, response) => {
-        try {
-            const raids = await RaidModel.find({});
-            response.status(200);
-            response.send(raids);
+	app.get("/getRaids", validateJwtToken, async (request, response) => {
+		try {
+			const raids = await RaidModel.find({});
+			response.status(200);
+			response.send(raids);
 
-            console.log("GET:", raids);
-        } catch (error) {
-            response.status(500);
-            response.send("Something went wrong processing the request.");
-            console.error("getRaidsError:", error);
-        }
-    });
+			console.log("GET:", raids);
+		} catch (error) {
+			response.status(500);
+			response.send("Something went wrong processing the request.");
+			console.error("getRaidsError:", error);
+		}
+	});
 
-    app.post("/addRaid", validateJwtToken, async (request, response) => {
-        const newRaid = new RaidModel(request.body);
+	app.post("/addRaid", validateJwtToken, async (request, response) => {
+		const newRaid = new RaidModel(request.body);
 
-        try {
-            // Gets newest raid registered
-            let lastRaid = null;
-            await RaidModel.findOne({}, null, {sort: {timestamp: -1}}).then((res) => {
-                lastRaid = res;
-            });
+		try {
+			// Gets newest raid registered
+			let lastRaid = null;
+			await RaidModel.findOne({}, null, {sort: {timestamp: -1}}).then((res) => {
+				lastRaid = res;
+			});
 
-            if (lastRaid == null) {
-                await newRaid.save().then(() => {
-                    response.send({err: ""});
-                });
-            } else {
-                const isSameUsers = (users1, users2) => {
-                    users1.sort();
-                    users2.sort();
+			if (lastRaid == null) {
+				await newRaid.save().then(() => {
+					response.send({err: ""});
+				});
+			} else {
+				const isSameUsers = (users1, users2) => {
+					users1.sort();
+					users2.sort();
 
-                    return users1.every((user) => users2.includes(user));
-                };
+					return users1.every((user) => users2.includes(user));
+				};
 
-                // If the last raid was registered less
-                // than 10 seconds ago and it's players
-                // are the same as this one, then it's
-                // likely to be the same raid.
-                const timeDiff = Math.abs(newRaid.timestamp - lastRaid.timestamp);
-                if (timeDiff < 10000 && isSameUsers(lastRaid.users, newRaid.users)) {
-                    response.send({err: "duplicate raid"});
-                    return;
-                }
+				// If the last raid was registered less
+				// than 10 seconds ago and it's players
+				// are the same as this one, then it's
+				// likely to be the same raid.
+				const timeDiff = Math.abs(newRaid.timestamp - lastRaid.timestamp);
+				if (timeDiff < 10000 && isSameUsers(lastRaid.users, newRaid.users)) {
+					response.send({err: "duplicate raid"});
+					return;
+				}
 
-                await newRaid.save().then(() => {
-                    response.send({err: ""});
-                });
-            }
-        } catch (error) {
-            response.send({err: "something went wrong"});
+				await newRaid.save().then(() => {
+					response.send({err: ""});
+				});
+			}
+		} catch (error) {
+			response.send({err: "something went wrong"});
 
-            console.error("postRaidError:", error);
-        }
-    });
+			console.error("postRaidError:", error);
+		}
+	});
 }
 
 export default mapRaidEndpoints;
--- security/jwtTokenGenerator.js
+++ security/jwtTokenGenerator.js
@@ -1,42 +1,30 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 import TokenResponseModel from "../models/responseModels.js";
+import dotenv from "dotenv";
 
-const secretKey = "secret-key";
+dotenv.config();
+const secretKey = process.env.JWT_SECRET_KEY;
 const options = {expiresIn: "24h"};
-
 
 // Generates a JWT if the validation key is valid.
 // Returns an object containing success status, the token
 // and any error messages if failure to generate token.
 function generateJwtToken(validationKey) {
+	// Validate key sent
+	if (validationKey !== process.env.JWT_VALIDATION_KEY)
+		return new TokenResponseModel(false, null, "Invalid validation key.");
 
-    // Validate key sent
-    if (validationKey !== "PlaceboValidationKey")
-        return new TokenResponseModel(
-            false,
-            null,
-            "Invalid validation key."
-        );
+	// Generate the token
+	let response;
+	try {
+		const jwtToken = jwt.sign({}, secretKey, options);
 
-    // Generate the token
-    let response;
-    try {
-        const jwtToken = jwt.sign({}, secretKey, options);
+		response = new TokenResponseModel(true, jwtToken, null);
+	} catch (error) {
+		response = new TokenResponseModel(false, null, "There was an error creating the token: " + error.message);
+	}
 
-        response = new TokenResponseModel(
-            true,
-            jwtToken,
-            null
-        );
-    } catch (error) {
-        response = new TokenResponseModel(
-            false,
-            null,
-            "There was an error creating the token: " + error.message
-        );
-    }
-
-    return response;
+	return response;
 }
 
 export default generateJwtToken;
