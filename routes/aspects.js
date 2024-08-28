9a69d75d1c3e786af33f51b612a5d9bc3b2eb3e2
Antunes <36316038+zpdh@users.noreply.github.com>
2024-08-28 20:56:03 -0300

    feat: add filtering to getAspects endpoint

--- routes/aspects.js
+++ routes/aspects.js
@@ -1,36 +1,39 @@
+import UserModel from "../models/userModel.js";
+import validateJwtToken from "../security/jwtTokenValidator.js";
+
 function mapAspectEndpoints(app) {
-  app.get("/getAspectsOwed", async (request, response) => {
-    try {
-      const aspects = await UserModel.find({});
-      response.status(200);
-      response.send(aspects);
+    app.get("/getAspectsOwed", async (request, response) => {
+        try {
+            // Get 10 users with the highest aspect count
+            const aspects = await UserModel.find({}).sort({aspects: -1}).limit(10);
 
-      console.log("GET:", aspects);
-    } catch (error) {
-      response.status(500);
-      response.send("Something went wrong processing the request.");
-      console.error("getAspectsError:", error);
-    }
-  });
+            response.status(200);
+            response.send(aspects);
 
-  app.post("/giveAspect", validateJwtToken, async (request, response) => {
-    try {
-      request.body.users.forEach((user) => {
-        userModel
-          .updateOne(
-            { user: user },
-            { $inc: { aspects: -1 } },
-            { upsert: true }
-          )
-          .then((res) => {
-            console.log(user, "receieved an aspect");
-          });
-      });
-      response.send({ err: "" });
-    } catch (error) {
-      response.status(500);
-      response.send({ err: "something went wrong" });
-      console.error("giveAspectError:", error);
-    }
-  });
+            console.log("GET:", aspects);
+        } catch (error) {
+            response.status(500);
+            response.send("Something went wrong processing the request.");
+            console.error("getAspectsError:", error);
+        }
+    });
+
+    app.post("/giveAspect", validateJwtToken, async (request, response) => {
+        try {
+            request.body.users.forEach((user) => {
+                UserModel.updateOne(
+                    {user: user},
+                    {$inc: {aspects: -1}},
+                    {upsert: true}
+                ).then((res) => {
+                    console.log(user, "received an aspect");
+                });
+            });
+            response.send({err: ""});
+        } catch (error) {
+            response.status(500);
+            response.send({err: "something went wrong"});
+            console.error("giveAspectError:", error);
+        }
+    });
 }
