8a9e76c72afaea9bcdf2c5c77fb0ed0bdf2085f8
Antunes <36316038+zpdh@users.noreply.github.com>
2024-08-30 04:41:11 -0300

    docs: better document jwt functions

--- security/jwtTokenValidator.js
+++ security/jwtTokenValidator.js
@@ -4,18 +4,32 @@ import "../config.js";
 // Needs to match the token in the generator. Store it in a .env or .json for reusability.
 const secretKey = process.env.JWT_SECRET_KEY;
 
+/**
+ * Checks if the token provided in the request's headers is valid.
+ * If token is invalid, return status code 401 with an error message,
+ * else, return void.
+ * @param {Request} request
+ * @param {Response} response
+ * @param {Function} next
+ * @returns {void}
+ */
 // Checks if the token provided in the request's headers is valid.
 // If token is invalid, return status code 401 with an error message.
 function validateJwtToken(request, response, next) {
-	// Get authorization headers and extract token from "Bearer <token>"
-	if (request.headers["authorization"] == null) return response.status(401).json({error: "No token provided"});
-	const token = request.headers["authorization"].split(" ")[1];
+    if (request.headers["authorization"] == null)
+        return response.status(401).json({ error: "No token provided" });
 
-	jwt.verify(token, secretKey, (err) => {
-		if (err) return response.status(401).json({error: "Invalid token provided"});
+    // Get authorization headers and extract token from "Bearer <token>"
+    const token = request.headers["authorization"].split(" ")[1];
 
-		next(); // Goes to next step (function execution)
-	});
+    jwt.verify(token, secretKey, (err) => {
+        if (err)
+            return response
+                .status(401)
+                .json({ error: "Invalid token provided" });
+
+        next(); // Goes to next step (function execution)
+    });
 }
 
 export default validateJwtToken;
