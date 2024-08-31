d94847e0244d628f92b27cd2cf51b1eed2d3f1b4
Antunes <36316038+zpdh@users.noreply.github.com>
2024-08-31 17:37:39 -0300

    feat: add tome model

---
+++ models/tomeModel.js
@@ -0,0 +1,10 @@
+﻿import mongoose, {Schema} from "mongoose";
+
+const schema = mongoose.Schema;
+const tomeSchema = new Schema({
+    username: {type: String, required: true},
+});
+
+const TomeModel = mongoose.model("Tome", tomeSchema);
+
+export default TomeModel;
