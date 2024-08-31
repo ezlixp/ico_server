f60f248e0a27b1b0fbbcae6c25c088ce5835a054
Antunes <36316038+zpdh@users.noreply.github.com>
2024-08-31 17:51:46 -0300

    feat: add endpoint to get users in tome list

--- models/tomeModel.js
+++ models/tomeModel.js
@@ -3,6 +3,7 @@
 const schema = mongoose.Schema;
 const tomeSchema = new Schema({
     username: {type: String, required: true},
+    dateAdded: {type: Date, required: true, default: Date.now()}
 });
 
 const TomeModel = mongoose.model("Tome", tomeSchema);
---
+++ routes/tomes.js
@@ -0,0 +1,26 @@
+﻿import {request, response} from "express";
+import validateJwtToken from "../security/jwtTokenValidator.js";
+import TomeModel from "../models/tomeModel.js";
+
+/**
+ * Maps all tome-related endpoints
+ * @param {Express} app
+ */
+export function mapTomeEndpoints(app) {
+    app.get("/tomes", async (request, response) => {
+        try {
+            // Get 10 users ordered by time added
+            const tomeList = await TomeModel
+                .find({})
+                .sort({dateAdded: -1});
+
+            response.status(200).send(tomeList);
+
+            console.log("GET:", tomeList)
+        } catch (error) {
+            response.status(500);
+            response.send("Something went wrong processing the request.");
+            console.error("getTomesError:", error);
+        }
+    });
+}
