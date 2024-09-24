9090eb5163702777d887d1b09adc92eee770cda2
helooo1233 <henrygoals2007@gmail.com>
2024-09-24 00:11:55 -0400

    feat: start work on using expressws server

--- app.ts
+++ app.ts
@@ -1,13 +1,5 @@
 import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
+
 const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: true,
-        methods: ["GET", "POST"],
-    },
-});
-export { io, server };
+
 export default app;
--- routes/aspects.ts
+++ routes/aspects.ts
@@ -1,42 +1,60 @@
 import { Request, Response, Router } from "express";
 import UserModel from "../models/userModel.js";
 import validateJwtToken from "../security/jwtTokenValidator.js";
-import { io } from "../app.js";
-
+import express from "express";
+import app from "../app.js";
+import expressWs from "express-ws";
 let messageIndex = 0;
 
 type aspectEventArgs = {
     player: string;
 };
 
-io.of("/aspects").on("connection", (socket) => {
-    console.log(socket.id);
-    socket.data.messageIndex = messageIndex;
-    socket.on("give_aspect", async (args: aspectEventArgs) => {
-        if (socket.data.messageIndex === messageIndex) {
-            ++messageIndex;
-            ++socket.data.messageIndex;
-            console.log(args.player, messageIndex);
-            UserModel.updateOne(
-                { username: args.player },
-                { $inc: { aspects: -1 } },
-                {
-                    upsert: true,
-                    collation: { locale: "en", strength: 2 },
-                }
-            ).then(() => {
-                console.log(args.player, "received an aspect");
-            });
-        } else {
-            ++socket.data.messageIndex;
-            if (socket.data.messageIndex < messageIndex - 10) socket.data.messageIndex = messageIndex;
-        }
-    });
-    socket.on("sync", () => {
-        socket.data.messageIndex = messageIndex;
-    });
-    socket.on("debug_index", () => {
-        console.log(socket.data.messageIndex);
+// io.of("/aspects").on("connection", (socket) => {
+//     console.log(socket.id);
+//     socket.data.messageIndex = messageIndex;
+//     socket.on("give_aspect", async (args: aspectEventArgs) => {
+//         if (socket.data.messageIndex === messageIndex) {
+//             ++messageIndex;
+//             ++socket.data.messageIndex;
+//             console.log(args.player, messageIndex);
+//             UserModel.updateOne(
+//                 { username: args.player },
+//                 { $inc: { aspects: -1 } },
+//                 {
+//                     upsert: true,
+//                     collation: { locale: "en", strength: 2 },
+//                 }
+//             ).then(() => {
+//                 console.log(args.player, "received an aspect");
+//             });
+//         } else {
+//             ++socket.data.messageIndex;
+//             if (socket.data.messageIndex < messageIndex - 10) socket.data.messageIndex = messageIndex;
+//         }
+//     });
+//     socket.on("sync", () => {
+//         socket.data.messageIndex = messageIndex;
+//     });
+//     socket.on("debug_index", () => {
+//         console.log(socket.data.messageIndex);
+//     });
+// });
+
+/**
+ * Maps websocket for aspect route
+ * Note that this can be on aspect router as well, but it is split
+ * so that the middleware can be excluded from routes in aspect router
+ */
+expressWs(app);
+const aspectSocketRouter = Router();
+// aspectSocketRouter.use(validateJwtToken);
+// TODO: figure out authentication for websockets
+
+aspectSocketRouter.ws("/aspects", (ws, req) => {
+    console.log("on");
+    ws.on("message", (args) => {
+        console.log(args);
     });
 });
 
@@ -83,4 +101,5 @@ aspectRouter.post("/aspects", validateJwtToken, async (request: Request, respons
     }
 });
 
+export { aspectSocketRouter as socketRouter };
 export default aspectRouter;
