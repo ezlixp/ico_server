bf356b42f882c8e532cf53a1e538c6eb02d92691
helooo1233 <71606730+helooo1233@users.noreply.github.com>
2024-10-01 01:16:08 -0400

    fix: fix wynn message emit not emitting content

--- routes/discord.ts
+++ routes/discord.ts
@@ -5,16 +5,21 @@ import "../config.js";
  * Maps all discord-related endpoints
  */
 
+type wynnMessageArgs = {
+    content: string;
+    extractedUsernames: string[];
+};
+
 let messageIndex = 0;
 io.of("/discord").on("connection", (socket) => {
     console.log(socket.id + " discord");
     socket.data.messageIndex = messageIndex;
-    socket.on("wynnMessage", async (content: string) => {
+    socket.on("wynnMessage", async (args: wynnMessageArgs) => {
         if (socket.data.messageIndex === messageIndex) {
             ++messageIndex;
             ++socket.data.messageIndex;
-            console.log(content);
-            io.of("/discord").emit("wynnMessage", content);
+            console.log(args.content, args.extractedUsernames);
+            io.of("/discord").emit("wynnMessage", args.content);
         } else {
             ++socket.data.messageIndex;
             if (socket.data.messageIndex < messageIndex - 10) socket.data.messageIndex = messageIndex;
