b10ce85d5645b94090a7bbc27c3b94e1ef545780
helooo1233 <71606730+helooo1233@users.noreply.github.com>
2024-09-30 20:58:00 -0400

    feat: add discord to game direction to websocket

--- routes/discord.ts
+++ routes/discord.ts
@@ -14,16 +14,20 @@ let messageIndex = 0;
 io.of("/discord").on("connection", (socket) => {
     console.log(socket.id + " discord");
     socket.data.messageIndex = messageIndex;
-    socket.on("send", async (args: sendEventArgs) => {
+    socket.on("wynnMessage", async (args: sendEventArgs) => {
         if (socket.data.messageIndex === messageIndex) {
             ++messageIndex;
             ++socket.data.messageIndex;
             console.log(args.username, args.message);
-            io.of("/discord").emit("message", args.username + ": " + args.message);
+            io.of("/discord").emit("wynnMessage", args.username + ": " + args.message);
         } else {
             ++socket.data.messageIndex;
             if (socket.data.messageIndex < messageIndex - 10) socket.data.messageIndex = messageIndex;
         }
+    });
+    socket.on("discordMessage", (args) => {
+        console.log(args);
+        io.of("/discord").emit("discordMessage", args);
     });
     socket.on("sync", () => {
         socket.data.messageIndex = messageIndex;
