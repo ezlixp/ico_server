b5fbb62e30a98a4854af1365c42a3b4aaf0825be
helooo1233 <71606730+helooo1233@users.noreply.github.com>
2024-10-01 22:27:07 -0400

    fix: fix waitlist dates being innacurate

--- models/waitlistModel.ts
+++ models/waitlistModel.ts
@@ -1,4 +1,4 @@
-﻿import mongoose, {Model, Schema} from "mongoose";
+﻿import mongoose, { Model, Schema } from "mongoose";
 
 interface IWaitlist extends Document {
     username: String;
@@ -6,8 +6,8 @@ interface IWaitlist extends Document {
 }
 
 const waitlistSchema: Schema<IWaitlist> = new Schema({
-    username: {type: String, required: true},
-    dateAdded: {type: Date, required: true, default: Date.now()},
+    username: { type: String, required: true },
+    dateAdded: { type: Date, required: true, default: Date.now },
 });
 
 const WaitlistModel: Model<IWaitlist> = mongoose.model("Waitlist", waitlistSchema);
