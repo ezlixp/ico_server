1035705e2163954b521a108fa6e104aa76de5bbf
pixlze <henrygoals2007@gmail.com>
2024-10-26 02:31:11 -0400

    feat: add decrement aspect endpoint

--- routes/aspects.ts
+++ routes/aspects.ts
@@ -1,6 +1,8 @@
 import { Request, Response, Router } from "express";
 import { UsernametoUUID } from "../services/ConvertMinecraftUser.js";
 import UserModel from "../models/userModel.js";
+import updateAspects from "../sockets/updateAspects.js";
+import validateJwtToken from "../security/jwtTokenValidator.js";
 
 /**
  * Maps all aspect-related endpoints.
@@ -40,5 +42,18 @@ aspectRouter.get("/aspects/:username", async (request: Request<{ username: strin
         console.error("getSpecificAspectsError:", error);
     }
 });
+
+aspectRouter.post(
+    "/aspects",
+    validateJwtToken,
+    async (request: Request<{}, {}, { username: string }>, response: Response) => {
+        try {
+            updateAspects(request.body.username);
+        } catch (error) {
+            response.status(500).send({ error: "Something went wrong processing the request." });
+            console.error("postAspectError:", error);
+        }
+    }
+);
 
 export default aspectRouter;
