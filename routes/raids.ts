b1a6baf1ad44cded4d5a8f9eb3254ccfa4bf9c90
helooo1233 <71606730+helooo1233@users.noreply.github.com>
2024-10-28 21:17:58 -0400

    fix: fix empty usernames for raid leaderboard

--- routes/raids.ts
+++ routes/raids.ts
@@ -1,6 +1,7 @@
 ﻿import RaidModel from "../models/raidModel.js";
 import { Request, Response, Router } from "express";
 import UserModel from "../models/userModel.js";
+import { UUIDtoUsername } from "../services/ConvertMinecraftUser.js";
 
 /**
  * Maps all raid-related endpoints base route: /raids.
@@ -22,9 +23,13 @@ raidRouter.get("/", async (request: Request, response: Response) => {
 
 raidRouter.get("/leaderboard", async (request: Request, response: Response) => {
     try {
-        const topUsers = await UserModel.find({}, { raids: true, username: true })
-            .sort({ raids: "descending" })
-            .limit(10);
+        const topUsers = await UserModel.find({}).sort({ raids: "descending" }).limit(10);
+        // TODO implement mojang api bulk uuid to username call
+        for (let i = 0; i < topUsers.length; i++) {
+            topUsers[i] = await topUsers[i]
+                .$set({ username: await UUIDtoUsername(topUsers[i].uuid.toString()) })
+                .save();
+        }
         response.send(topUsers);
     } catch (error) {
         response.status(500).send({ error: "Something went wrong processing the request" });
