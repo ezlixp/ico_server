a24cc7595f024abb0ac2f8f932738e443de2e45c
pixlze <henrygoals2007@gmail.com>
2024-12-01 15:11:40 -0500

    feat: add checks for if user is in guild before adding to lists

---
+++ services/wynncraftApiClient.ts
@@ -0,0 +1,20 @@
+async function getPlayersGuildAsync(username: string) {
+    const apiUrl = `https://api.wynncraft.com/v3/player/${username}`;
+
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    return data.guild ?? null;
+}
+
+export default async function checkIfPlayerIsGuildAsync(username: string, guildId: string) {
+    const guild = await getPlayersGuildAsync(username);
+
+    if (guild != null) {
+        if (guild.uuid == guildId) {
+            return true;
+        }
+    }
+
+    return false;
+}
