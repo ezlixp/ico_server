09d7cb9d85b6d823d4fdbde7b412e347511a796b
helooo1233 <71606730+helooo1233@users.noreply.github.com>
2024-12-17 23:29:59 -0500

    fix: fix array elements being optional fix: fix inviting self fix: fix delete server config not working

--- types/express.d.ts
+++ types/express.d.ts
@@ -1,17 +1,18 @@
 declare namespace Express {
     export interface Request {
         guildId?: string;
-        serverQuery?: import("mongoose").Query<
-            | (import("mongoose").Document<unknown, {}, import("../models/serverConfigModel.ts").ISeverConfig> &
-                  import("../models/serverConfigModel.ts").ISeverConfig)
-            | null,
-            import("mongoose").Document<unknown, {}, import("../models/serverConfigModel.ts").ISeverConfig> &
-                import("../models/serverConfigModel.ts").ISeverConfig,
-            {},
-            import("../models/serverConfigModel.ts").ISeverConfig,
-            "findOne",
-            {}
-        >;
+        // serverQuery?: import("mongoose").Query<
+        //     | (import("mongoose").Document<unknown, {}, import("../models/serverConfigModel.ts").ISeverConfig> &
+        //           import("../models/serverConfigModel.ts").ISeverConfig)
+        //     | null,
+        //     import("mongoose").Document<unknown, {}, import("../models/serverConfigModel.ts").ISeverConfig> &
+        //         import("../models/serverConfigModel.ts").ISeverConfig,
+        //     {},
+        //     import("../models/serverConfigModel.ts").ISeverConfig,
+        //     "findOne",
+        //     {}
+        // >;
+        serverId?: number;
         serverConfig?: import("mongoose").Document<unknown, {}, import("../models/serverConfigModel.ts").ISeverConfig> &
             import("../models/serverConfigModel.ts").ISeverConfig;
     }
