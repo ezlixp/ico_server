c3e5f4d2e1926ef33f74c95e6b67751545f9463c
helooo1233 <henrygoals2007@gmail.com>
2024-12-22 22:27:25 -0500

    refactor: remove invites and make listening channel and broadcasting channel singular

--- models/serverConfigModel.ts
+++ models/serverConfigModel.ts
@@ -1,9 +1,9 @@
 import mongoose, { Schema } from "mongoose";
 
-interface IListeningChannel {
-    channelId: number;
-    guildId: string;
-}
+// interface IListeningChannel {
+//     channelId: number;
+//     guildId: string;
+// }
 
 export interface ISeverConfig {
     wynnGuildId: string;
@@ -13,10 +13,10 @@ export interface ISeverConfig {
     raidsChannel: number;
     warChannel: number;
     privilegedRoles: number[];
-    invites: string[];
-    outgoingInvites: string[];
-    listeningChannels: IListeningChannel[];
-    broadcastingChannels: number[];
+    // invites: string[];
+    // outgoingInvites: string[];
+    listeningChannel: number;
+    broadcastingChannel: number;
 }
 
 const serverConfigSchema: Schema<ISeverConfig> = new Schema({
@@ -27,33 +27,22 @@ const serverConfigSchema: Schema<ISeverConfig> = new Schema({
     raidsChannel: { type: Number, required: true, default: -1 },
     warChannel: { type: Number, required: true, default: -1 },
     privilegedRoles: { type: [{ type: Number, required: true }], required: true, default: [] },
-    invites: { type: [{ type: String, required: true }], required: true, default: [] },
-    outgoingInvites: { type: [{ type: String, required: true }] },
-    listeningChannels: {
-        type: [
-            {
-                channelId: { type: Number, required: true },
-                guildId: { type: String, required: true },
-            },
-        ],
-        required: true,
-        default: [],
-    },
-    broadcastingChannels: { type: [{ type: Number, required: true }], required: true, default: [] },
+    // invites: { type: [{ type: String, required: true }], required: true, default: [] },
+    // outgoingInvites: { type: [{ type: String, required: true }] },
+    listeningChannel: { type: Number, required: true, default: -1 },
+    broadcastingChannel: { type: Number, required: true, default: -1 },
 });
 
 /**
- * @property {string} wynnGuildId                              - The guild id a server is connected to.
- * @property {number} discordGuildId                           - The server id a guild is connected to.
- * @property {number} tomeChannel                              - The channel id for tomes, or -1 if not specified.
- * @property {number} layoffsChannel                           - The channel id for layoffs, or -1 if not specified.
- * @property {number} raidsChannel                             - The channel id for raids, or -1 if not specified.
- * @property {number} warChannel                               - The channel id for wars, or -1 if not specified.
- * @property {number[]} privelagedRoles                        - The roles in the discord server with additional permissions.
- * @property {string[]} invites                                - The guild ids of guild servers inviting a bridge.
- * @property {string[]} outgoingInvites                        - The guild ids of outgoing invites.
- * @property {IListeningChannel[]} listeningChannels         - The channel ids that are listening for messages.
- * @property {number[]} broadcastingChannels                   - The channel ids to broadcast bridge messages to, not necessarily in the same server.
+ * @property {string} wynnGuildId         - The guild id a server is connected to.
+ * @property {number} discordGuildId      - The server id a guild is connected to.
+ * @property {number} tomeChannel         - The channel id for tomes, or -1 if not specified.
+ * @property {number} layoffsChannel      - The channel id for layoffs, or -1 if not specified.
+ * @property {number} raidsChannel        - The channel id for raids, or -1 if not specified.
+ * @property {number} warChannel          - The channel id for wars, or -1 if not specified.
+ * @property {number[]} privilegedRoles   - The roles in the discord server with additional permissions.
+ * @property {number} listeningChannel    - The channel ids that are listening for messages.
+ * @property {number} broadcastingChannel - The channel ids to broadcast bridge messages to, not necessarily in the same server.
  */
 const ServerConfigModel = mongoose.model("Server Config", serverConfigSchema);
 
