1504fcab2493571e4acb9a8874eede139a3e20fc
pixlze <henrygoals2007@gmail.com>
2024-12-29 00:10:16 -0500

    fix: fix incorrectly assuming duplicate keys chore: clean up logging

--- middleware/verifyGuild.middleware.ts
+++ middleware/verifyGuild.middleware.ts
@@ -4,7 +4,6 @@ import { guildDatabases } from "../models/guildDatabaseModel.js";
 
 export default function verifyGuild(request: GuildRequest, response: Response, next: NextFunction) {
     if (!(request.params.wynnGuildId in guildDatabases) && request.params.wynnGuildId !== "*") {
-        console.log(request.params);
         response
             .status(400)
             .send({ error: "This guild is not configured. In order to configure a guild, contact a developer." });
