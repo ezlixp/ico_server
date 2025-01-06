d17eb5a3f71875ce2f2de262a8edf7d1c6c8bc29
Antunes <36316038+zpdh@users.noreply.github.com>
2025-01-06 20:53:07 -0300

    feat: start implementing repository pattern

---
+++ repositories/Base/BaseRepository.ts
@@ -0,0 +1,64 @@
+﻿import {BaseModel} from "../../models/BaseModel.js";
+import {Model} from "mongoose";
+
+export type FilterOptions = Record<string, unknown>;
+
+export interface IRepository<T> {
+    findOne(options: FilterOptions): Promise<T | null>;
+
+    find(options: FilterOptions): Promise<T[]>;
+
+    create(data: Partial<T>): Promise<void>;
+
+    update(options: FilterOptions, data: Partial<T>): Promise<void>
+
+    deleteOne(options: FilterOptions): Promise<void>;
+}
+
+// TODO: Add custom errors
+export abstract class BaseRepository<T extends BaseModel> implements IRepository<T> {
+    protected constructor(private model: Model<T>) {
+    }
+
+    async findOne(options: FilterOptions): Promise<T | null> {
+        try {
+            return await this.model.findOne(options).exec();
+        } catch (err) {
+            throw new Error();
+        }
+    }
+
+    async find(options: FilterOptions): Promise<T[]> {
+        try {
+            return await this.model.find(options).exec();
+        } catch (err) {
+            throw new Error();
+        }
+    }
+
+    async create(data: Partial<T>): Promise<void> {
+        try {
+            const createdEntity = new this.model(data);
+            await createdEntity.save();
+        } catch (err) {
+            throw new Error();
+        }
+    }
+
+    async update(options: FilterOptions, data: Partial<T>): Promise<void> {
+        try {
+            await this.model.findOneAndUpdate(options, data, {new: true}).exec();
+        } catch (err) {
+            throw new Error();
+        }
+    }
+
+    async deleteOne(options: FilterOptions): Promise<void> {
+        try {
+            await this.model.findOneAndDelete(options).exec();
+        } catch (err) {
+            throw new Error();
+        }
+    }
+
+}
