1a16e6e2e4cacaf5924cd2e355e29626e3f20a61
Antunes <36316038+zpdh@users.noreply.github.com>
2025-01-06 20:59:28 -0300

    feat: add initial UserRepository.ts implementation

--- models/userModel.ts
+++ models/userModel.ts
@@ -1,18 +1,20 @@
-import mongoose, { Model, Schema } from "mongoose";
+import mongoose, {Model, Schema} from "mongoose";
+import {BaseModel} from "./BaseModel.js";
 
-export interface IUser {
+export interface IUser extends BaseModel {
     uuid: string;
     wynnGuildId: string;
     blocked: string[];
     muted: boolean;
 }
 
-// TODO: figure out how to make collation default without having to add it to each request
 const userSchema: Schema<IUser> = new Schema({
-    uuid: { type: String, required: true },
-    wynnGuildId: { type: String, required: true },
-    blocked: { type: [String], required: true, default: [] },
-    muted: { type: Boolean, required: true, default: false },
+    uuid: {type: String, required: true},
+    wynnGuildId: {type: String, required: true},
+    blocked: {type: [String], required: true, default: []},
+    muted: {type: Boolean, required: true, default: false},
+}, {
+    collation: {locale: "en", strength: 2}
 });
 
 const UserModel: Model<IUser> = mongoose.model("User", userSchema);
---
+++ repositories/UserRepository.ts
@@ -0,0 +1,8 @@
+﻿import {BaseRepository} from "./base/BaseRepository.js";
+import UserModel, {IUser} from "../models/userModel.js";
+
+export class UserRepository extends BaseRepository<IUser> {
+    constructor() {
+        super(UserModel);
+    }
+}
