b7c49412dfe781ee1512a6749bf1e6733f1e678d
Antunes <36316038+zpdh@users.noreply.github.com>
2025-01-06 22:49:19 -0300

    refactor: move database creation functions to GuildDatabaseCreator.ts class

--- models/guildDatabaseModel.ts
+++ models/guildDatabaseModel.ts
@@ -1,10 +1,8 @@
-import mongoose, {Model} from "mongoose";
-import guildUserSchema, {IGuildUser} from "./schemas/guildUserSchema.js";
-import raidSchema, {IRaid} from "./schemas/raidSchema.js";
-import tomeSchema, {ITome} from "./schemas/tomeSchema.js";
-import waitlistSchema, {IWaitlist} from "./schemas/waitlistSchema.js";
-import ValidationModel from "./validationModel.js";
-import {GuildDatabaseFactory} from "./factories/GuildDatabaseFactory.js";
+import {Model} from "mongoose";
+import {IGuildUser} from "./schemas/guildUserSchema.js";
+import {IRaid} from "./schemas/raidSchema.js";
+import {ITome} from "./schemas/tomeSchema.js";
+import {IWaitlist} from "./schemas/waitlistSchema.js";
 
 export interface IGuildDatabase {
     GuildUserModel: Model<IGuildUser>;
@@ -29,45 +27,3 @@ export const guildNames: { [key: string]: string } = {
 };
 
 export const guildDatabases: IGuildDatabases = {};
-
-/**
- * Creates collection in each guild's database for each of the following schemas:
- *
- * {@link guildUserSchema}
- * {@link raidSchema}
- * {@link tomeSchema}
- * {@link waitlistSchema}
- */
-export function newDatabase(wynnGuildName: string, wynnGuildId: string) {
-    if (Object.keys(guildIds).indexOf(wynnGuildName) != -1) {
-        console.warn("trying to register already existing database:", wynnGuildName);
-        return;
-    }
-    guildIds[wynnGuildName] = wynnGuildId;
-    guildNames[wynnGuildId] = wynnGuildName;
-    registerDatabase([wynnGuildName, wynnGuildId]);
-}
-
-export function registerDatabase(value: [string, string]) {
-    console.log(value);
-
-    const dbName = value[0];
-    const db = mongoose.connection.useDb(dbName);
-    const databaseFactory = GuildDatabaseFactory.create(db);
-
-    guildDatabases[value[1]] = databaseFactory.createDatabase();
-}
-
-export default async function registerDatabases() {
-    const guilds = await ValidationModel.find().exec();
-    for (let i = 0; i < guilds.length; ++i) {
-        const name = guilds[i].wynnGuildName.replaceAll(" ", "+");
-        const guildId = guilds[i].wynnGuildId;
-        guildIds[name] = guildId;
-        guildNames[guildId] = name;
-    }
-    console.log("registered guilds:", JSON.stringify(guildIds, null, 2));
-    Object.entries(guildIds).forEach((value) => {
-        registerDatabase(value);
-    });
-}
