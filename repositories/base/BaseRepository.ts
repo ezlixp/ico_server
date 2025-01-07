2fdcedc1b0f6708299d96aedfd08ea763df5e35c
Antunes <36316038+zpdh@users.noreply.github.com>
2025-01-06 21:07:59 -0300

    feat: add custom error classes

--- repositories/base/BaseRepository.ts
+++ repositories/base/BaseRepository.ts
@@ -1,5 +1,6 @@
 ﻿import {BaseModel} from "../../models/BaseModel.js";
 import {Model} from "mongoose";
+import {DatabaseError} from "../../errors/DatabaseError.js";
 
 export type FilterOptions = Record<string, unknown>;
 
@@ -15,7 +16,6 @@ export interface IRepository<T> {
     deleteOne(options: FilterOptions): Promise<void>;
 }
 
-// TODO: Add custom errors
 export abstract class BaseRepository<T extends BaseModel> implements IRepository<T> {
     protected constructor(private model: Model<T>) {
     }
@@ -24,7 +24,7 @@ export abstract class BaseRepository<T extends BaseModel> implements IRepository
         try {
             return await this.model.findOne(options).exec();
         } catch (err) {
-            throw new Error();
+            throw new DatabaseError();
         }
     }
 
@@ -32,7 +32,7 @@ export abstract class BaseRepository<T extends BaseModel> implements IRepository
         try {
             return await this.model.find(options).exec();
         } catch (err) {
-            throw new Error();
+            throw new DatabaseError();
         }
     }
 
@@ -41,7 +41,7 @@ export abstract class BaseRepository<T extends BaseModel> implements IRepository
             const createdEntity = new this.model(data);
             await createdEntity.save();
         } catch (err) {
-            throw new Error();
+            throw new DatabaseError();
         }
     }
 
@@ -49,7 +49,7 @@ export abstract class BaseRepository<T extends BaseModel> implements IRepository
         try {
             await this.model.findOneAndUpdate(options, data, {new: true}).exec();
         } catch (err) {
-            throw new Error();
+            throw new DatabaseError();
         }
     }
 
@@ -57,7 +57,7 @@ export abstract class BaseRepository<T extends BaseModel> implements IRepository
         try {
             await this.model.findOneAndDelete(options).exec();
         } catch (err) {
-            throw new Error();
+            throw new DatabaseError();
         }
     }
 
