58923d13b04eed7e99c30f5fec4754956c99c0a6
Antunes <36316038+zpdh@users.noreply.github.com>
2025-01-06 23:25:15 -0300

    feat: add exception handler middleware

---
+++ middleware/errorHandler.middleware.ts
@@ -0,0 +1,13 @@
+﻿import {ErrorRequestHandler} from "express";
+import {AppError} from "../errors/base/AppError.js";
+import {ErrorResponse, HttpErrorResponse} from "../communication/responses/ErrorResponse.js";
+
+export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    if (err instanceof AppError) {
+        res.status(err.statusCode).send(ErrorResponse.createWithError(err));
+    } else {
+        console.error("Unexpected error:", err);
+
+        res.status(500).send(HttpErrorResponse.InternalServerError)
+    }
+};
