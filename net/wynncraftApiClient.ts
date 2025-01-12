87ff38414dabedfbd454f18bc374ba34ab92891b
Antunes <36316038+zpdh@users.noreply.github.com>
2025-01-12 20:09:27 -0300

    refactor: refactor wynncraftApiClient.ts

--- net/wynncraftApiClient.ts
+++ net/wynncraftApiClient.ts
@@ -1,27 +1,17 @@
-import { usernameToUuid } from "./mojangApiClient.js";
-import { insertDashes } from "../utils/uuidUtils.js";
+import {usernameToUuid} from "./mojangApiClient.js";
+import {insertDashes} from "../utils/uuidUtils.js";
 
 async function getPlayersGuildAsync(username: string) {
     const apiUrl = `https://api.wynncraft.com/v3/player/${insertDashes(await usernameToUuid(username))}`;
     const response = await fetch(apiUrl);
-    try {
-        const data = await response.json();
+    const data = await response.json();
 
-        return data.guild ?? null;
-    } catch (e) {
-        console.log("getPlayerGuildAsyncError:", username, e);
-        return null;
-    }
+    return data.guild ?? null;
 }
 
-export default async function checkIfPlayerIsGuildAsync(username: string, wynnGuildId: string) {
+export default async function checkIfPlayerIsInGuildAsync(username: string, wynnGuildId: string) {
     const guild = await getPlayersGuildAsync(username);
 
-    if (guild != null) {
-        if (guild.uuid == wynnGuildId) {
-            return true;
-        }
-    }
+    return guild != null && guild.uuid == wynnGuildId;
 
-    return false;
 }
