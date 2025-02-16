5a71a36a2b52bea1bfcd39a7a060fb4cbc819f37
Pedro <36316038+zpdh@users.noreply.github.com>
2025-02-15 23:32:20 -0300

    refactor: refactor mojangApiClient.ts to improve readability

--- communication/httpClients/mojangApiClient.ts
+++ communication/httpClients/mojangApiClient.ts
@@ -1,4 +1,5 @@
-import { NotFoundError } from "../../errors/implementations/notFoundError.js";
+import {NotFoundError} from "../../errors/implementations/notFoundError.js";
+import {HttpClientError} from "../../errors/implementations/httpClientError.js";
 
 /** Caches username to UUID conversions from Mojang api.*/
 const usernameUuidMap: { [key: string]: { uuid: string; timestamp: number } } = {};
@@ -10,20 +11,23 @@ export async function usernameToUuid(username: string): Promise<string> {
     if (usernameUuidMap[username] && Date.now() - usernameUuidMap[username].timestamp < 1.728e9) {
         return usernameUuidMap[username].uuid;
     }
+
     const url = `https://api.mojang.com/users/profiles/minecraft/${username}`;
-    try {
-        const response = await fetch(url);
-        if (!response.body) throw "empty mojang username to uuid response";
-        const res: { id: string } = await response.json();
-        if (res.id) {
-            uuidUsernameMap[res.id] = { username: username, timestamp: Date.now() };
-            usernameUuidMap[username] = { uuid: res.id, timestamp: Date.now() };
-            return res.id;
-        }
-    } catch (error) {
-        console.error("username to uuid error:", error);
+    const apiResponse = await fetch(url);
+
+    if (!apiResponse.body) {
+        throw new HttpClientError("empty mojang username to uuid response");
     }
-    throw new NotFoundError("Could not find minecraft account with provided username.");
+
+    const responseBody: { id: string, errorMessage: string } = await apiResponse.json();
+
+    if (responseBody.errorMessage) {
+        throw new HttpClientError(responseBody.errorMessage);
+    }
+
+    uuidUsernameMap[responseBody.id] = {username: username, timestamp: Date.now()};
+    usernameUuidMap[username] = {uuid: responseBody.id, timestamp: Date.now()};
+    return responseBody.id;
 }
 
 export async function uuidToUsername(uuid: string): Promise<string> {
@@ -31,18 +35,21 @@ export async function uuidToUsername(uuid: string): Promise<string> {
     if (uuidUsernameMap[uuid] && Date.now() - uuidUsernameMap[uuid].timestamp < 1.728e9) {
         return uuidUsernameMap[uuid].username;
     }
+
     const url = `https://sessionserver.mojang.com/session/minecraft/profile/${uuid}`;
-    try {
-        const response = await fetch(url);
-        if (!response.body) throw "empty mojang uuid to username response";
-        const res: { name: string } = await response.json();
-        if (res.name) {
-            uuidUsernameMap[uuid] = { username: res.name, timestamp: Date.now() };
-            usernameUuidMap[res.name] = { uuid: uuid, timestamp: Date.now() };
-            return res.name;
-        }
-    } catch (error) {
-        console.error("uuid to username error:", error);
+    const apiResponse = await fetch(url);
+
+    if (!apiResponse.body) {
+        throw new HttpClientError("empty mojang uuid to username response");
     }
-    throw new NotFoundError("Could not find minecraft account with provided uuid.");
+
+    const responseBody: { name: string, errorMessage: string } = await apiResponse.json();
+
+    if (responseBody.errorMessage) {
+        throw new HttpClientError(responseBody.errorMessage);
+    }
+
+    uuidUsernameMap[uuid] = {username: responseBody.name, timestamp: Date.now()};
+    usernameUuidMap[responseBody.name] = {uuid: uuid, timestamp: Date.now()};
+    return responseBody.name;
 }
