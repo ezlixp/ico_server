a56c75f3322d4b27195f06f64fc8a58a04b6986c
Pedro <36316038+zpdh@users.noreply.github.com>
2025-02-20 02:54:41 -0300

    feat: start implementing user service

---
+++ src/user/user.service.ts
@@ -0,0 +1,15 @@
+﻿import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './user.schema';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async getByUuid(uuid: string): Promise<User | null> {
+    return this.userModel.findOne({ uuid });
+  }
+}
