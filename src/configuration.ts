5785b5e03ffc79088f766477bf15d8e86a7ef964
Pedro <36316038+zpdh@users.noreply.github.com>
2025-02-20 02:27:07 -0300

    feat: add initial user schema

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,1 +1,1 @@
-﻿export const API_VERSION = 1;
+﻿export const API_VERSION = 3;
---
+++ src/user/user.schema.ts
@@ -0,0 +1,21 @@
+﻿import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema({ collation: { locale: 'en', strength: 2 } })
+export class User {
+  @Prop({ unique: true })
+  uuid: string;
+
+  @Prop()
+  wynnGuildId: string;
+
+  @Prop()
+  blockedUsers: string[];
+
+  @Prop({ default: false })
+  isMuted: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
