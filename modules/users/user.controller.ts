f2aef77d875ef4b3ba7eec7158e33e5b3ad7221b
Pedro <36316038+zpdh@users.noreply.github.com>
2025-02-22 21:35:13 -0300

    refactor: refactor user controller to user decorators

--- modules/users/user.controller.ts
+++ modules/users/user.controller.ts
@@ -1,55 +1,44 @@
-﻿import { injectable, singleton } from 'tsyringe';
-import { UserService } from './user.service.js';
-import { Request } from 'express';
-import { DefaultResponse } from '../../communication/responses/defaultResponse.js';
-import { IUser } from './user.model.js';
+﻿import { UserService } from './user.service.js';
 import {
-    HttpDelete,
-    HttpGet,
-    HttpPost,
-} from '../../decorators/http.methods.js';
+    Body,
+    Delete,
+    Get,
+    HttpCode,
+    JsonController,
+    Param,
+    Post,
+} from 'routing-controllers';
 
-@injectable()
-@singleton()
+@JsonController('/user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
-    @HttpGet('/blocked/:uuid')
-    async getBlockedList(
-        request: Request<{ uuid: string }>,
-        response: DefaultResponse<string[]>,
-    ): Promise<void> {
-        const uuid = request.params.uuid.replaceAll('-', '');
-        const blockedList = await this.userService.getBlockedList({ uuid });
+    @Get('/blocked/:uuid')
+    async getBlockedList(@Param('uuid') uuid: string) {
+        uuid = uuid.replaceAll('-', '');
 
-        response.status(200).send(blockedList);
+        return await this.userService.getBlockedList({ uuid });
     }
 
-    @HttpPost('/blocked/:uuid')
+    @Post('/blocked/:uuid')
+    @HttpCode(201)
     async addUserToBlockedList(
-        request: Request<{ uuid: string }, unknown, { toBlock: string }>,
-        response: DefaultResponse<IUser>,
+        @Param('uuid') uuid: string,
+        @Body() body: { toBlock: string },
     ) {
-        const toBlock = request.body.toBlock;
-        const uuid = request.params.uuid.replaceAll('-', '');
-        const updatedUser = await this.userService.addToBlockedList(
-            { uuid },
-            toBlock,
-        );
+        uuid = uuid.replaceAll('-', '');
 
-        response.status(200).send(updatedUser);
+        return this.userService.addToBlockedList({ uuid }, body.toBlock);
     }
 
-    @HttpDelete('/blocked/:uuid/:toRemove')
+    @Delete('/blocked/:uuid/:toRemove')
+    @HttpCode(204)
     async removeUserFromBlockedList(
-        request: Request<{ uuid: string; toRemove: string }>,
-        response: DefaultResponse,
+        @Param('uuid') uuid: string,
+        @Param('toRemove') toRemove: string,
     ) {
-        const uuid = request.params.uuid.replaceAll('-', '');
-        const toRemove = request.params.toRemove;
+        uuid = uuid.replaceAll('-', '');
 
         await this.userService.removeFromBlockedList({ uuid }, toRemove);
-
-        response.status(204).send();
     }
 }
