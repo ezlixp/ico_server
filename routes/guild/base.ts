324e9295fc7e29d88ed1eac73776aca5867ddc58
pixlze <henrygoals2007@gmail.com>
2025-06-22 00:28:41 -0400

    feat: add base route for checking guild registered

---
+++ routes/guild/base.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import onlineRouter from "./online.js";
+import raidRouter from "./raids.js";
+import tomeRouter from "./tomes.js";
+import waitlistRouter from "./waitlist.js";
+import { GuildRequest } from "../../communication/requests/guildRequest.js";
+import { DefaultResponse } from "../../communication/responses/defaultResponse.js";
+import { guildNames } from "../../models/entities/guildDatabaseModel.js";
+import { NotFoundError } from "../../errors/implementations/notFoundError.js";
+
+const guildRouter = Router({ mergeParams: true });
+
+guildRouter.get("/:wynnGuildId", async (request: GuildRequest, response: DefaultResponse) => {
+    const wynnGuildId = request.params.wynnGuildId;
+    if (wynnGuildId in guildNames) {
+        response.send(guildNames[wynnGuildId]);
+    }
+    throw new NotFoundError("Guild not found.");
+});
+
+guildRouter.use("/online", onlineRouter);
+guildRouter.use("/raids", raidRouter);
+guildRouter.use("/tomes", tomeRouter);
+guildRouter.use("/waitlist", waitlistRouter);
+
+export default guildRouter;
