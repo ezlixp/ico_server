4b889a0411dba6d78b1170e9ccc779660c6ca9de
helooo1233 <71606730+helooo1233@users.noreply.github.com>
2025-07-10 22:49:28 -0400

    refactor: remove stack trace from error handler middleware

--- src/middleware/errorHandler.middleware.ts
+++ src/middleware/errorHandler.middleware.ts
@@ -6,7 +6,7 @@ export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
     if (res.headersSent) next(err);
     console.warn("error from route:", req.originalUrl, "method:", req.method);
     if (err instanceof AppError) {
-        console.error(err.statusCode, err.message, err.stack);
+        console.error(err.statusCode, err.message);
 
         res.status(err.statusCode).send(ErrorResponse.createWithError(err));
     } else {
