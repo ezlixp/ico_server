41136b2c2e3a6003edfc537ea447aae055a8a291
pixlze <henrygoals2007@gmail.com>
2025-07-12 17:47:16 -0400

    feat: use github env variables

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 const envFile: string = ".env." + process.env.NODE_ENV;
 export const API_VERSION = "v3";
 
-dotenv.config({ path: path.resolve(process.cwd(), envFile) });
+if (process.env.NODE_ENV !== "test") dotenv.config({ path: path.resolve(process.cwd(), envFile) });
+console.log(process.env.NODE_ENV);
 
 export default dotenv;
