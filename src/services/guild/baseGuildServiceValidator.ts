adb2f66f84ddd16342f1de5075ba5ceefb067d9e
ezlixp <henrygoals2007@gmail.com>
2025-08-07 01:07:43 -0400

    refactor: 404 for not found guild

--- src/services/guild/baseGuildServiceValidator.ts
+++ src/services/guild/baseGuildServiceValidator.ts
@@ -1,13 +1,14 @@
-import { ValidationError } from "../../errors/implementations/validationError";
 import { guildDatabases } from "../../models/entities/guildDatabaseModel";
 import { GuildErrors } from "../../errors/messages/guildErrors";
+import { NotFoundError } from "../../errors/implementations/notFoundError";
 
 export abstract class BaseGuildServiceValidator {
     protected constructor() {}
 
     validateGuild(wynnGuildId: string) {
         if (!(wynnGuildId in guildDatabases) && wynnGuildId !== "*") {
-            throw new ValidationError(GuildErrors.NOT_CONFIGURED);
+            throw new NotFoundError(GuildErrors.NOT_CONFIGURED);
         }
     }
 }
+
