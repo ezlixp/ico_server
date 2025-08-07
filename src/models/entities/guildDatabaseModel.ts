4dea4d0594c5140b94a2585017624e8061f14ac6
ezlixp <henrygoals2007@gmail.com>
2025-08-06 23:55:00 -0400

    chore: remove unecessary comment

--- src/models/entities/guildDatabaseModel.ts
+++ src/models/entities/guildDatabaseModel.ts
@@ -15,11 +15,9 @@ export interface IGuildDatabases {
     [key: string]: IGuildDatabase;
 }
 
-/*Supported guilds:
-    Idiot Co: b250f587-ab5e-48cd-bf90-71e65d6dc9e7
-*/
 export const guildIds: { [key: string]: string } = {};
 
 export const guildNames: { [key: string]: string } = {};
 
 export const guildDatabases: IGuildDatabases = {};
+
